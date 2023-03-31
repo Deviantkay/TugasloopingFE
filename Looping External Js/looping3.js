6195d27eee1d03156c0c53e3ea5df6babf6f558e
Deviantkay <51223543+Deviantkay@users.noreply.github.com>
2023-03-31 12:17:33 +0700

    Tugas Js
    Added some External Js to show if we use external js instead internal script on html

---
+++ Looping External Js/looping3.js
@@ -0,0 +1,7 @@
+for (let i = 0; i <= 20; i++) {
+    if (i % 2 == 0) {
+        document.write(i + " adalah bilangan genap" + "<br>");
+    } else {
+        document.write(i + " adalah bilangan ganjil" + "<br>");
+    }
+}
